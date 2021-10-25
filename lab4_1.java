dfe92daeeccd3ee78f8eaa0b924f9639622b4687
Уйгууна <46619571+uyguuna@users.noreply.github.com>
2021-10-26 02:14:24 +0900

    Update lab4_1.java

--- lab4_1.java
+++ lab4_1.java
@@ -2,9 +2,9 @@ public class lab4_1{
         public static void main(String[] args) {
             try {
                 System.out.println("0");
-                throw new RuntimeException("Непроверяемая ошибка");
-            } catch (RuntimeException e) { // исключение перехвачено
-                System.out.println("1 " + e); // исключение обработано
+                throw new RuntimeException("ошибка");
+            } catch (RuntimeException e) { 
+                System.out.println("1 " + e); 
             }
             System.out.println("2");
         }
